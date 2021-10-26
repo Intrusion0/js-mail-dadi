540f233eaf278f7e7063081904c30c8e293f2438
Mario <lombardomario01@gmail.com>
2021-10-26 15:39:52 +0200

    Mail completed

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,4 +3,24 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 Qui, come detto, NON possiamo usare metodi nuovi sugli array ma lo facciamo con le cose viste questa mattina.
 */
 
-console.log("hey ciaoo eccomi, sono MAIL");
+// Creo la lista delle email che possono accedere
+const emailList = ["lombardomario01@gmail.com", "lucagenovese0@hotmail.it", "carlochiesa23@alice.it", "filippocarlona50@gmail.com"];
+
+// Chiedo all'utente la sua email
+let userEmail = prompt("Inserisci la tua email");
+
+
+// Controllo che sia nella lista di chi può accedere e stampo il messaggio sull'esito del controllo
+for (let i = 0; i < emailList.length; i++){
+
+    if (userEmail === emailList[i]){
+        const contEmail = document.querySelector("div.container");
+        const checkResult = document.createElement("div");
+        checkResult.classList.add("result");
+        checkResult.append("La tua email è presente nella lista: ", userEmail);
+        contEmail.append(checkResult);
+    }
+    else {
+        alert("Attenzione!! non puoi accedere. L'email inserita non è presente nella lista.");
+    }
+}
