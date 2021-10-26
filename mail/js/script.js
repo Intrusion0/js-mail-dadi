4c042a36c51e3cbbbd7dbeb606b876ad4b85b8ff
Mario <lombardomario01@gmail.com>
2021-10-26 14:34:39 +0200

    Created files

---
+++ mail/js/script.js
@@ -0,0 +1,6 @@
+/* MAIL 
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (che hai fatto tu precedentemente), stampa un messaggio appropriato sull’esito del controllo.
+Qui, come detto, NON possiamo usare metodi nuovi sugli array ma lo facciamo con le cose viste questa mattina.
+*/
+
+console.log("hey ciaoo eccomi, sono MAIL");
